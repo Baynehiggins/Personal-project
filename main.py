f4fde42f0eb91d02097667497c4387734b79b5cd
Baynehiggins <Bayne_higgins@biswashington.org>
2025-02-07 23:19:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 student = input("enter student name: ")
 grade = int(input("enter student test scores:"))
+average = sum(num_list)/len(num_list)
+
 
 print(student)
 print(grade)
 
-print(f"{student}'s grade is {grade}")
+print(f"{student}'s grade is {grade}") 
